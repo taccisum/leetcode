e3f737a7b06b38cc6a66c019cafe46ceb0f77435
taccisum <liaojinfeng6937@dingtalk.com>
2023-01-16 10:01:19 +0800

    *: optimize code

--- 547.py
+++ 547.py
@@ -6,22 +6,20 @@ from typing import List
 def findCircleNum(isConnected: List[List[int]]) -> int:
     width=len(isConnected[0]); height=len(isConnected)        # 计算长宽
 
-    c=0; stack=[];
+    c=0; stack=[]; visited=set()
     for i in range(height):
         for j in range(width):
             if isConnected[i][j] == 1:
-                # 开始 dfs
-                clear=set()
-                stack.append(i)     # 第 i 个人入栈
+                stack.append(i)     # 根节点入栈，开始 dfs
                 while len(stack) > 0:
-                    x = stack.pop(); y = j
-                    if x in clear: continue
+                    x=stack.pop(); y=j      # 直接从 y=j 开始检索
+                    if x in visited: continue   # 避免重复遍历
+                    visited.add(x)     # 记忆已经检索过的行
                     while y < width:
                         if isConnected[x][y] == 1:
-                            isConnected[x][y] = 0
-                            if x != y and x not in clear: stack.append(y)
+                            isConnected[x][y] = 0   # 将已经访问过的节点置为 0，避免重复访问
+                            if x != y: stack.append(y)      # 新的分支节点入栈
                         y+=1
-                    clear.add(x)     # 记忆已经检索过的行，提高性能
                 c+=1
                 break
     return c
